38ae297ac5ce1bc47dacd2a6b027d1aacf67d70e
premierinfotech609 <premierinfotech609@gmail.com>
2019-07-17 16:34:14 +0530

    only admin user login and Dashboard count show

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -2,60 +2,74 @@
 
 @section('content')
 
-    <div class="row">
+<div class="row">
 
-        <div class="col-md-8 col-md-offset-2">
+    <div class="col-md-8 col-md-offset-2">
 
-            <div class="panel panel-default">
-                <div class="panel-heading">{{ trans('labels.frontend.auth.login_box_title') }}</div>
+        <div class="panel panel-default">
+            <div class="panel-heading">{{ trans('labels.frontend.auth.login_box_title') }}</div>
 
-                <div class="panel-body">
+            <div class="panel-body">
 
-                    {{ Form::open(['route' => 'frontend.auth.login', 'class' => 'form-horizontal']) }}
+                {{ Form::open(['route' => 'frontend.auth.login.user', 'class' => 'form-horizontal']) }}
 
-                    <div class="form-group">
-                        {{ Form::label('email', trans('validation.attributes.frontend.register-user.email'), ['class' => 'col-md-4 control-label']) }}
-                        <div class="col-md-6">
-                            {{ Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.register-user.email')]) }}
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
+                <div class="form-group">
+                    {{ Form::label('email', trans('validation.attributes.frontend.register-user.email'), ['class' => 'col-md-4 control-label']) }}
+                    <div class="col-md-6">
+                        {{ Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.register-user.email')]) }}
 
-                    <div class="form-group">
-                        {{ Form::label('password', trans('validation.attributes.frontend.register-user.password'), ['class' => 'col-md-4 control-label']) }}
-                        <div class="col-md-6">
-                            {{ Form::input('password', 'password', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.register-user.password')]) }}
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
+                        @if ($errors->has('email'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                        @endif
+                    </div><!--col-md-6-->
 
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                            <div class="checkbox">
-                                <label>
-                                    {{ Form::checkbox('remember') }} {{ trans('labels.frontend.auth.remember_me') }}
-                                </label>
-                            </div>
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
+                </div><!--form-group-->
 
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                            {{ Form::submit(trans('labels.frontend.auth.login_button'), ['class' => 'btn btn-primary', 'style' => 'margin-right:15px']) }}
+                <div class="form-group">
+                    {{ Form::label('password', trans('validation.attributes.frontend.register-user.password'), ['class' => 'col-md-4 control-label']) }}
+                    <div class="col-md-6">
+                        {{ Form::input('password', 'password', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.register-user.password')]) }}
+                        @if ($errors->has('password'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                        @endif
+                    </div><!--col-md-6-->
 
-                            {{ link_to_route('frontend.auth.password.reset', trans('labels.frontend.passwords.forgot_password')) }}
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
+                </div><!--form-group-->
 
-                    {{ Form::close() }}
+                <div class="form-group">
+                    <div class="col-md-6 col-md-offset-4">
+                        <div class="checkbox">
+                            <label>
+                                {{ Form::checkbox('remember') }} {{ trans('labels.frontend.auth.remember_me') }}
+                            </label>
+                        </div>
+                    </div><!--col-md-6-->
 
-                    <div class="row text-center">
+                </div><!--form-group-->
 
-                    </div>
-                </div><!-- panel body -->
+                <div class="form-group">
+                    <div class="col-md-6 col-md-offset-4">
+                        {{ Form::submit(trans('labels.frontend.auth.login_button'), ['class' => 'btn btn-primary', 'style' => 'margin-right:15px']) }}
 
-            </div><!-- panel -->
+                        {{ link_to_route('frontend.auth.password.reset', trans('labels.frontend.passwords.forgot_password')) }}
+                    </div><!--col-md-6-->
+                </div><!--form-group-->
 
-        </div><!-- col-md-8 -->
+                {{ Form::close() }}
 
-    </div><!-- row -->
+                <div class="row text-center">
+
+                </div>
+            </div><!-- panel body -->
+
+        </div><!-- panel -->
+
+    </div><!-- col-md-8 -->
+
+</div><!-- row -->
 
 @endsection
