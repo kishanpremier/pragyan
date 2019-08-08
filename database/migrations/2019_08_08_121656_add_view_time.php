bae105539a692359b8d63866d153511273ea69a7
programmer110798 <jatin.premierinfotech@gmail.com>
2019-08-08 17:56:28 +0530

    Migration File

--- database/migrations/2019_08_08_121656_add_view_time.php
+++ database/migrations/2019_08_08_121656_add_view_time.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class RemoveSchoolIdFromSubject extends Migration
+class AddViewTime extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class RemoveSchoolIdFromSubject extends Migration
      */
     public function up()
     {
-        Schema::table('subject', function (Blueprint $table) {
-            $table->dropColumn('school_id');
+        Schema::table('video_count', function (Blueprint $table) {
+            $table->string('view_time')->default('null');
         });
     }
 
@@ -25,8 +25,8 @@ class RemoveSchoolIdFromSubject extends Migration
      */
     public function down()
     {
-        Schema::table('subject', function (Blueprint $table) {
-            $table->string('school_id');
+        Schema::table('video_count', function (Blueprint $table) {
+            $table->dropColumn('view_time');
         });
     }
 }
